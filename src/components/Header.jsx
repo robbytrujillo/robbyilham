70d35d2c9bfa34bc0d991462f45f615d9eeace34
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-02 14:42:47 +0700

    Add Header anda socialMedia page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import profilePicture from '../assets/photo-profile1.png';
 import '../styles/Header.css';
 // import icon
 import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 
 
 function Header() {
@@ -13,6 +16,10 @@ function Header() {
         <p>Fullstack Developer - Learner - Researcher</p>
         <div className='socialMedia'>
             <a href=''><FaInstagram /></a>
+            <a href=''><FaGithub /></a>
+            <a href=''><FaLinkedin /></a>
+            <a href=''><FaFacebookF /></a>
+            
         </div>
     </div>   
     </header>
