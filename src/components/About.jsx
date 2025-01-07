4f3eea383babd602ce4b962c78a4a545af6ed115
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-07 09:35:42 +0700

    About section portofoliosatu

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,38 @@
-import '../styles/About.css'
+import '../styles/About.css';
+
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaPhp } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaVuejs } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { FaLaravel } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { SiPostman } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaBootstrap } from "react-icons/fa";
+
 
 function About() {
   return (
     <section id="about">
-        
+        <div className="wrapper">
+            <h3>About</h3>
+            <p>lorem ipsum dolor sit lorem ipsum dolor sit</p> 
+            <p>lorem ipsum dolor sit lorem 
+              ipsum dolor sit lorem ipsum dolor 
+              sit lorem ipsum dolor sit lorem ipsum 
+              dolor sit lorem ipsum dolor sit</p>
+            <h4>Programming Language & Tools</h4>
+            <div className="skills">
+              <FaHtml5 /> <FaCss3Alt /> <IoLogoJavascript /> <FaPhp /> <FaJava />
+              <FaReact /> <FaVuejs /> <FaPython /> <FaLaravel /> <FaGitAlt /> <FaGithub /> 
+              <SiPostman /> <RiTailwindCssFill /> <FaBootstrap />
+            </div>
+        </div>
     </section>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
    <header>
     <div className="header-jumbotron">
         <img src={profilePicture} />
-        <h3>Robby Ilham</h3>
+        <h3>Robby Ilhamkusuma</h3>
         <p>Fullstack Developer - Learner - Researcher</p>
         <div className='socialMedia'>
             <a href=''><FaInstagram /></a>
