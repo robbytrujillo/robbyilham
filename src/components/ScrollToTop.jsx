01aee4aa48645138ae0ade1c2d4b0ed206f726a6
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-13 14:42:12 +0700

    Add scroll to top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -6,12 +6,6 @@ function ScrollToTop() {
   useEffect(() => {
     window.scrollTo(0,0)
   },[location])
-
-//     return (
-//     <div>
-//       Scroll To Top
-//     </div>
-//   )
 }
 
 export default ScrollToTop
