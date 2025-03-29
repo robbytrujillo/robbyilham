9d68792feb1e0a02fa69556ed3337e8d80567aab
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-29 14:33:27 +0700

    Introduction Portfolio Robby

--- src/data/DataPortfolio.jsx
+++ src/data/DataPortfolio.jsx
@@ -1,35 +1,35 @@
 // import aset gambar 
 import portfolio8 from '../assets/portfolio8.png'
 import portfolio9 from '../assets/portfolio9.png'
-import portfolio3 from '../assets/portfolio3.png'
-import portfolio4 from '../assets/portfolio4.png'
+import portfolio10 from '../assets/portfolio10.png'
+import portfolio11 from '../assets/portfolio11.png'
 import portfolio5 from '../assets/portfolio5.png'
 import portfolio6 from '../assets/portfolio6.png'
 
 export  const portfolioList = [
     {
         id: '1',
-        title: 'Website Portfolio #1',
+        title: 'Homcoming IHBS',
         image: portfolio8,
         skill: 'html, css, javascript, bootsrap, php'
     },
     {
         id: '2',
-        title: 'Website Portfolio #2',
+        title: 'MY Blog',
         image: portfolio9,
         skill: 'vuejs, html, css, express, node'
     },
     {
         id: '3',
-        title: 'Website Portfolio #3',
-        image: portfolio3,
-        skill: 'sveltejs, html, css, express, node'
+        title: 'Health IHBS',
+        image: portfolio10,
+        skill: 'html, css, javascript, bootsrap, php'
     },
     {
         id: '4',
-        title: 'Website Portfolio #4',
-        image: portfolio4,
-        skill: 'phython, Django'
+        title: 'Inventory IHBS',
+        image: portfolio11,
+        skill: 'html, css, javascript, bootsrap, php'
     },
     {
         id: '5',
