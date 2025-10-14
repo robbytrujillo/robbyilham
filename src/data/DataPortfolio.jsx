7e3500c1d1068063aa40ba67513ae6ca339221aa
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-14 09:32:49 +0700

    my porto extended

--- src/data/DataPortfolio.jsx
+++ src/data/DataPortfolio.jsx
@@ -1,58 +1,84 @@
-// import aset gambar 
-import portfolio8 from '../assets/portfolio8.png'
-import portfolio9 from '../assets/portfolio9.png'
-import portfolio10 from '../assets/portfolio10.png'
-import portfolio11 from '../assets/portfolio11.png'
-import portfolio7 from '../assets/portfolio7.png'
-import portfolio12 from '../assets/certificate-home.png'
+// import aset gambar
+import portfolio8 from "../assets/portfolio8.png";
+import portfolio9 from "../assets/portfolio9.png";
+import portfolio10 from "../assets/portfolio10.png";
+import portfolio11 from "../assets/portfolio11.png";
+import portfolio7 from "../assets/portfolio7.png";
+import portfolio12 from "../assets/certificate-home.png";
+import portfolio13 from "../assets/desa-digital2.png";
+import portfolio14 from "../assets/cari-hotel.png";
 
-export  const portfolioList = [
-    {
-        id: '1',
-        title: 'Homcoming IHBS',
-        image: portfolio8,
-        skill: 'html, css, javascript, bootsrap, php',
-        site: 'https://homecoming.ihbs.sch.id',
-        information: 'This system was built to manage the return of students or Islamic boarding school students from boarding schools or pondok, specifically at IHBS'
-    },
-    {
-        id: '2',
-        title: 'MY Blog',
-        image: portfolio9,
-        skill: 'vuejs, html, css, express, node',
-        site: 'https://robbyblogs.netlify.app',
-        information: 'This system was built for my own personal project using local storage as its static data'
-    },
-    {
-        id: '3',
-        title: 'Health IHBS',
-        image: portfolio10,
-        skill: 'html, css, javascript, bootsrap, php',
-        site: 'https://health.ihbs.sch.id/',
-        information: 'This system was built to record data on students who are sick and before and after being treated by health workers in the IHBS environment'
-    },
-    {
-        id: '4',
-        title: 'Inventory IHBS',
-        image: portfolio11,
-        skill: 'html, css, javascript, bootsrap, php',
-        site: 'https://inventory.ihbs.sch.id',
-        information: 'This system was built to record equipment that is an inventory for use by students and employees in the IHBS environment'
-    },
-    {
-        id: '5',
-        title: 'HRIS IHBS',
-        image: portfolio7,
-        skill: 'Nuxt JS, Laravel',
-        site: 'https://hris-ihbs.netlify.app/login/',
-        information: 'This system is a prototype built to manage HR and employee data in the IHBS environment'
-    },
-    {
-        id: '6',
-        title: 'Certificate IHBS',
-        image: portfolio12,
-        skill: 'html, css, javascript, bootsrap, php',
-        site: 'https://certificate.ihbs.sch.id',
-        information: 'This system was built to manage the search for activity certificates at IHBS which are at IHBS to be printed in PDF format'
-    }
-]
+export const portfolioList = [
+  {
+    id: "1",
+    title: "Homcoming IHBS",
+    image: portfolio8,
+    skill: "html, css, javascript, bootsrap, php",
+    site: "https://homecoming.ihbs.sch.id",
+    information:
+      "This system was built to manage the return of students or Islamic boarding school students from boarding schools or pondok, specifically at IHBS",
+  },
+  {
+    id: "2",
+    title: "MY Blog",
+    image: portfolio9,
+    skill: "vuejs, html, css, express, node",
+    site: "https://robbyblogs.netlify.app",
+    information:
+      "This system was built for my own personal project using local storage as its static data",
+  },
+  {
+    id: "3",
+    title: "Health IHBS",
+    image: portfolio10,
+    skill: "html, css, javascript, bootsrap, php",
+    site: "https://health.ihbs.sch.id/",
+    information:
+      "This system was built to record data on students who are sick and before and after being treated by health workers in the IHBS environment",
+  },
+  {
+    id: "4",
+    title: "Inventory IHBS",
+    image: portfolio11,
+    skill: "html, css, javascript, bootsrap, php",
+    site: "https://inventory.ihbs.sch.id",
+    information:
+      "This system was built to record equipment that is an inventory for use by students and employees in the IHBS environment",
+  },
+  {
+    id: "5",
+    title: "HRIS IHBS",
+    image: portfolio7,
+    skill: "Nuxt JS, Laravel",
+    site: "https://hris-ihbs.netlify.app/login/",
+    information:
+      "This system is a prototype built to manage HR and employee data in the IHBS environment",
+  },
+  {
+    id: "6",
+    title: "Certificate IHBS",
+    image: portfolio12,
+    skill: "html, css, javascript, bootsrap, php",
+    site: "https://certificate.ihbs.sch.id",
+    information:
+      "This system was built to manage the search for activity certificates at IHBS which are at IHBS to be printed in PDF format",
+  },
+  {
+    id: "7",
+    title: "Desa Digital",
+    image: portfolio13,
+    skill: "html, css, javascript, react, laravel",
+    site: "https://desa.robbyilham.com/",
+    information:
+      "Information and communication technology-based systems for managing data, providing public services, and empowering the economy at the village level.",
+  },
+  {
+    id: "8",
+    title: "Cari Hotel",
+    image: portfolio14,
+    skill: "html, css, javascript, tailwind, laravel",
+    site: "https://carihotel.robbyilham.com/",
+    information:
+      "An application used to search for and book hotel accommodations. This application allows users to compare prices, check availability, and make reservations independently from their mobile phone or computer.",
+  },
+];
--- src/pages/DetailPortfolio.jsx
+++ src/pages/DetailPortfolio.jsx
@@ -1,35 +1,42 @@
 import { useParams, Navigate } from "react-router-dom";
 import "../styles/DetailPortfolio.css";
-import { portfolioList } from "../data/DataPortfolio"
+import { portfolioList } from "../data/DataPortfolio";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollToTop from "../components/ScrollToTop";
+import { Link } from "react-router-dom";
 
 function DetailPortfolio() {
   const { id } = useParams();
   const data = portfolioList.find((item) => item.id === id);
   if (data == undefined) {
-    return <Navigate to='/page-not-found' />
+    return <Navigate to="/page-not-found" />;
   }
 
   // console.log(data);
   return (
     // bungkus dengan fragmen
-    <> 
-     <Navbar />
-    <section id="detail-portfolio">
-      <div className="wrapper">
-        <h1>{data.title}</h1>
-        <img src={data.image} />
-        <p className="skill"><b>Skills:</b> {data.skill}</p>
-        <p className="site">{data.site}</p>
-        <p className="information">{data.information}</p>
-      </div>
-    </section>
-    <Footer />
-    <ScrollToTop />
+    <>
+      <Navbar />
+      <section id="detail-portfolio">
+        <div className="wrapper">
+          <h1>{data.title}</h1>
+          <img src={data.image} />
+          <p className="skill">
+            <b>Skills:</b> {data.skill}
+          </p>
+          <p className="site">
+            <Link to={data.site} target="_blank">
+              {data.site}
+            </Link>
+          </p>
+          <p className="information">{data.information}</p>
+        </div>
+      </section>
+      <Footer />
+      <ScrollToTop />
     </>
-  )
+  );
 }
 
-export default DetailPortfolio
+export default DetailPortfolio;
